7f3252001977d91d83d07eae0e7e928f9f80b08c
Atrotro <wanganzhouhh@163.com>
2024-06-14 09:35:32 +0000

    【修改说明】：支持mp3 title 非标解析
    Signed-off-by: Atrotro <wanganzhouhh@163.com>

--- libavformat/id3v2.c
+++ libavformat/id3v2.c
@@ -280,32 +280,32 @@ static int decode_str(AVFormatContext *s, AVIOContext *pb, int encoding,
         avio_seek(pb, -2, SEEK_CUR);
               
         switch (value) {
-            case 0xfffe:
-                av_log(s, AV_LOG_INFO, "BOM 0xfffe\n");
-                avio_rb16(pb);
-                get = avio_rl16;
-                break;
-            case 0xfeff:
-                av_log(s, AV_LOG_INFO, "BOM 0xfeff\n");
-                avio_rb16(pb);
-                break;
-            default:
-                av_log(s, AV_LOG_ERROR, "Notstandard BOM value\n");
-                left +=2;
-                get = avio_rl16;
+        case 0xfffe:
+            av_log(s, AV_LOG_INFO, "BOM 0xfffe\n");
+            avio_rb16(pb);
+            get = avio_rl16;
+            break;
+        case 0xfeff:
+            av_log(s, AV_LOG_INFO, "BOM 0xfeff\n");
+            avio_rb16(pb);
+            break;
+        default:
+            av_log(s, AV_LOG_ERROR, "Notstandard BOM value\n");
+            left += 2;
+            get = avio_rl16;
         }
 #else
-        switch(avio_rb16(pb)) {
-            case 0xfffe:
-                get = avio_rl16;
-            case 0xfeff:
-                break;
-            default:
-                av_log(s, AV_LOG_ERROR, "Incorrect BOM value\n");
-        ffio_free_dyn_buf(&dynbuf);
-        *dst = NULL;
-        *maxread = left;
-        return AVERROR_INVALIDDATA;
+        switch (avio_rb16(pb)) {
+        case 0xfffe:
+            get = avio_rl16;
+        case 0xfeff:
+            break;
+        default:
+            av_log(s, AV_LOG_ERROR, "Incorrect BOM value\n");
+            ffio_free_dyn_buf(&dynbuf);
+            *dst = NULL;
+            *maxread = left;
+            return AVERROR_INVALIDDATA;
         }
 #endif
         // fall-through
