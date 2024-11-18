1307bf5bf120f34464ccf508961f1649066f3f73
ziyugao <gaoziyu0614@outlook.com>
2024-11-18 10:19:17 +0800

    add ffmpeg fix
    Signed-off-by: ziyugao <gaoziyu0614@outlook.com>

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -3775,7 +3775,7 @@ static int mxf_get_next_track_edit_unit(MXFContext *mxf, MXFTrack *track, int64_
     b = track->original_duration;
 
     while (b - a > 1) {
-        m = (a + b) >> 1;
+        m = (a + (uint64_t)b) >> 1;
         if (mxf_edit_unit_absolute_offset(mxf, t, m, track->edit_rate, NULL, &offset, NULL, 0) < 0)
             return -1;
         if (offset < current_offset)
