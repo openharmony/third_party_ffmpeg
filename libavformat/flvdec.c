3563c1953595d09a7f21f1b410c8948f536e88e9
凌空先知 <2414106632@qq.com>
2025-03-05 09:36:48 +0800

    Fix the issue of screen distortion in TS and flv format file parsing
    Signed-off-by: 凌空先知 <2414106632@qq.com>

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -1421,8 +1421,9 @@ leave:
             !flv->broken_sizes) {
             av_log(s, AV_LOG_ERROR, "Packet mismatch %d %d %"PRId64"\n", last, orig_size + 11, flv->sum_flv_tag_size);
 #ifdef OHOS_OPT_COMPAT
-            // If there is an error in the AVIOContext and it is seekable, seek back to the original position,
-            // unreference the packet, and return -1 to indicate an error.
+The data request is seeable time,said to return precise position,and return mistakes-1
+            /* If there is an error in the AVIOContext and it is seekable, seek back to the original position */
+            /* unreference the packet, and return -1 to indicate an error */
             if (s->pb->error < 0 && (s->pb->seekable & AVIO_SEEKABLE_NORMAL)) {
                 avio_seek(s->pb, pos, SEEK_SET);
                 av_packet_unref(pkt);
