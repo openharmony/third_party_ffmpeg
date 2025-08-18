67a46c53c4afa5553bb7361962ad867f17aedd19
凌空先知 <2414106632@qq.com>
2025-08-18 11:09:36 +0800

    add av_vorbis_parse_frame_flags null pointer check
    Signed-off-by: 凌空先知 <2414106632@qq.com>

--- libavcodec/vorbis_parser.c
+++ libavcodec/vorbis_parser.c
@@ -218,7 +218,7 @@ int av_vorbis_parse_frame_flags(AVVorbisParseContext *s, const uint8_t *buf,
     int duration = 0;
 #ifdef OHOS_CHECK_NULL_PTR
     if (!s || !buf) {
-        av_log(s, AV_LOG_ERROR, "Invalid parameters: s=%p, buf=%p, buf_size=%d\n", s, buf, buf_size);
+        av_log(s, AV_LOG_ERROR, "Invalid parameters\n");
         return AVERROR_INVALIDDATA;
     }
 #endif
