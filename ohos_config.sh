7816dbdbfaaeb98059bdcb0ad8ec921828ae69d2
weixin_69925792 <1619121804@qq.com>
2025-09-26 18:46:36 +0800

    ffmpeg支持mjpeg解码 Signed-off-by: weixin_69925792 <1619121804@qq.com>

--- ohos_config.sh
+++ ohos_config.sh
@@ -55,9 +55,9 @@ FF_CONFIG_OPTIONS="
     --disable-vulkan
     --enable-demuxer=mp3,aac,ape,flac,ogg,wav,mov,mpegts,amr,amrnb,amrwb,matroska,flv,mpegps,asf,asf_o,srt,h264,webvtt,av3a,avi,mpegvideo,ac3
     --enable-muxer=mp4,h264,ipod,amr,mpegts,mp3,wav,flac,av3a,adts
-    --enable-parser=h263,h264,mpeg4video,vp8,vp9,mpegvideo
+    --enable-parser=h263,h264,mpeg4video,vp8,vp9,mpegvideo,mjpeg
     --enable-parser=mpegaudio,aac,aac_latm,av3a,amr,opus,ac3
-    --enable-decoder=h263,h264,mpeg2video,mpeg4,vp8,vp9
+    --enable-decoder=h263,h264,mpeg2video,mpeg4,vp8,vp9,mjpeg
     --enable-decoder=mp2,mp3,mp3float,aac,aac_latm,ape,flac,vorbis,opus,amrnb,amrwb,ac3
     --enable-decoder=png,bmp
     --enable-encoder=aac,aac_latm,opus,flac
